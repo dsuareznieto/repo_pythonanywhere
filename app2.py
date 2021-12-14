9c9cc52c131d39fe61d4a09841348e1bd1391e45
dsuareznieto <davidsuarez193@gmail.com>
2021-12-14 10:06:20 +0000

    nueva app

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Mamá mi primera aplicación</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>aplicación desde git</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 if __name__ == '__main__':
 
